80e2bc0fa4cf37f1cf23baac39df4691ed8f379f
Douglas Maitelli <dougmaitelli@gmail.com>
2018-01-14 16:30:54 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    "app.bundle": "./src/js/app.js",
+    "app.bundle.min": "./src/js/app.js"
+  },
+  devtool: "source-map",
+  output: {
+    path: __dirname + "/build",
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: {
+          loader: "url-loader"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    /*new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    }),*/
+    new HtmlWebpackPlugin({
+      chunks: ["app.bundle.min"],
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    inline: true
+  }
+};
